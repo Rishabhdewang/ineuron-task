669540a270e90f967730ee5d096ebb3f3ff6ceaa
Rishabh Dewang <48730812+Rishabhdewang@users.noreply.github.com>
2023-02-27 23:13:17 +0530

    Update migration.sql

--- migration.sql
+++ migration.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id bigserial PRIMARY KEY,
     username text UNIQUE, 
     password text,
-    deleted_at timestamp with time zone
+    deleted_at timestamp with time zone,
     created_at timestamp with time zone DEFAULT NOW(),
     updated_at timestamp with time zone
 );
