4c75a971248dd1af1f9f0ac0032fcfcc98e2b517
Rishabh Dewang <rishabhdewang@gmail.com>
2023-02-27 19:30:31 +0530

    deployment fix

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import app from "./src"
 
 dotenv.config();
 
-const PORT: number = process.env.NODE_ENV === "test" ? parseInt(process.env.TEST_PORT as string, 10) : parseInt(process.env.DEV_PORT as string, 10);
+const PORT: number = process.env.NODE_ENV === "test" ? 5000 as number : parseInt(process.env.PORT as string, 10);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
